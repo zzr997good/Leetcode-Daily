ffa918c1b002dd37907ab7987d59fff6a07d6459
Zhiren Zhou <zzr997good@tamu.edu>
2022-10-30 14:01:00 -0500

    update

--- 659.split-array-into-consecutive-subsequences.cpp
+++ 659.split-array-into-consecutive-subsequences.cpp
@@ -14,11 +14,13 @@ public:
         for(int num:nums){
             //明明有这个数，但是出现频率却为0，说明在这个数已经被用完了
             if(freq[num]==0) continue;
+            //这个数字被其他的数列所需要
             else if(need.count(num)&&need[num]>0){
                     freq[num]--;
                     need[num]--;
                     need[num+1]++;
             }
+            //这个数字不被其他的数列所需要，但是能和后面的其他数字构成新数列
             else if(freq[num+1]>0&&freq[num+2]>0){
                 freq[num]--;
                 freq[num+1]--;
