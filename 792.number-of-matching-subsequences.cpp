b943b1e1a58442895b7c1e5f1c1743312297ed25
Zhiren Zhou <zzr997good@tamu.edu>
2022-10-31 21:27:31 -0500

    update

---
+++ 792.number-of-matching-subsequences.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=792 lang=cpp
+ *
+ * [792] Number of Matching Subsequences
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        
+    }
+};
+// @lc code=end
+
