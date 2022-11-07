b334ca4d34b54c4d5ec961ebce37fd7cb0c8887d
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-07 14:15:54 -0600

    update

---
+++ 83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* fast=head, *slow=head;
+        while(fast!=nullptr){
+            if(fast==head||fast->val==slow->val) fast=fast->next;
+            else{
+                slow->next=fast;
+                slow=fast;
+                fast=fast->next;
+            }
+        }
+        if(slow) slow->next=nullptr;
+        return head;
+        
+    }
+};
+// @lc code=end
+
