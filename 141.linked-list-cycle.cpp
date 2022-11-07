7a904579c61aa00106bba3734870e4f709a4b63c
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-07 01:36:17 -0600

    update

--- 141.linked-list-cycle.cpp
+++ 141.linked-list-cycle.cpp
@@ -6,19 +6,16 @@
 
 // @lc code=start
 
-using namespace std;
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast!=NULL&&fast->next!=NULL){
+        ListNode* fast=head, *slow=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
             fast=fast->next->next;
             slow=slow->next;
             if(fast==slow) return true;
         }
         return false;
-
     }
 };
 // @lc code=end
---
+++ 142.linked-list-cycle-ii.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=142 lang=cpp
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast=head, *slow=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                slow=head;
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
+
