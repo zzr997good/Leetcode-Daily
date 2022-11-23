45649b193b5dbce81549a227a7e02077817da042
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-23 01:24:08 -0600

    update

--- 222.count-complete-tree-nodes.cpp
+++ 222.count-complete-tree-nodes.cpp
@@ -16,56 +16,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(root==nullptr) return 0;
         int depth=getDepth(root);
-        int left=0,right=(1<<depth)-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(isExist(root,depth,mid)) left=mid+1;
-            else right=mid-1;
+         //there are 2^depth-1 nodes above last floor
+        int lo=0,hi=(1<<depth);
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(exist(root,depth,mid)){
+                lo=mid+1;
+            }
+            else{
+                hi=mid;
+            }
         }
-        return (1<<depth)-1+(right-0+1);
+        return (1<<depth)-1+lo;
     }
 private:
     int getDepth(TreeNode* root){
-        TreeNode* cur=root;
         int depth=-1;
-        while(cur){
+        while(root!=nullptr){
             depth++;
-            cur=cur->left;
+            root=root->left;
         }
         return depth;
     }
-
-    int isExist(TreeNode* root, int depth, int index){
-        TreeNode* cur=root;
-        int left=0,right=(1<<depth)-1;
-        for(int i=0;i<depth;i++){
-            int mid=left+(right-left)/2;
-            if(index<=mid){
-                cur=cur->left;
-                right=mid;
+    bool exist(TreeNode* root, int depth,int no){
+        //The nodes in depth are labeled from 0 to 2^depth-1
+        int start=0,last=(1<<depth)-1;
+        while(start!=last){
+            int mid=start+(last-start)/2;
+            if(no<=mid){
+                root=root->left;
+                last=mid;
             }
             else{
-                cur=cur->right;
-                left=mid+1;
+                root=root->right;
+                start=mid+1;
             }
         }
-        return !(cur==nullptr);
+        return root!=nullptr;
     }
 };
 // @lc code=end
