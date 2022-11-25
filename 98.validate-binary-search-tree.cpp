4b24fde40997f0d7953e7bd4afdcb8f0d1eb8f6b
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-25 03:39:18 -0600

    update

--- 98.validate-binary-search-tree.cpp
+++ 98.validate-binary-search-tree.cpp
@@ -16,26 +16,60 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ 
+//  //Recursive way
+// class Solution {
+// public:
+//     //一边判断是否BST，一边记录最大最小值
+//     bool isValidBST(TreeNode* root) {
+//         if(root==nullptr) return true;
+//         bool left=isValidBST(root->left);
+//         if(!left) return false;
+//         int left_max;
+//         int left_min;
+//         if(root->left!=nullptr) {
+//             left_max=max;
+//             left_min=min;
+//             if(left_max>=root->val) return false;
+//         }
+//         else{
+//             left_max=root->val;
+//             left_min=root->val;
+//         }
+//         bool right=isValidBST(root->right);
+//         if(!right) return false;
+//         int right_max;
+//         int right_min;
+//         if(root->right!=nullptr){
+//             right_max=max;
+//             right_min=min;
+//             if(right_min<=root->val) return false;
+//         }
+//         else{
+//             right_max=root->val;
+//             right_min=root->val;
+//         }
+//         max=right_max;
+//         min=left_min;
+//         return true;
+//     }
+// private:
+//     int max;
+//     int min;
+//};
 
-//Traverse way
-class Solution {
+//中序遍历是单调递增数组    
+class Solution{
 public:
     bool isValidBST(TreeNode* root) {
-        return helper(root)[0];
+        if(root==nullptr) return true;
+        if(!isValidBST(root->left)) return false;
+        if(prev&&prev->val>=root->val) return false;
+        prev=root;
+        return isValidBST(root->right);
     }
 private:
-    vector<long long> helper(TreeNode* root){
-        //{isValid,max_val,min_val}
-        if(root==nullptr) return {1,LLONG_MIN,LLONG_MAX};
-        vector<long long> left=helper(root->left);
-        vector<long long> right=helper(root->right);
-        if(left[0]==0) return {0,0,0};
-        if(right[0]==0) return {0,0,0};
-        if(left[1]>=root->val||right[2]<=root->val) return {0,0,0};
-        int mx=(root->val>right[1]?root->val:right[1]);
-        int mn=(root->val<left[2]?root->val:left[2]);
-        return {1,mx,mn};
-    }
+    TreeNode* prev=nullptr;
 };
 // @lc code=end
 
